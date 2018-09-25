55d61485946d18a0acad15cc21da04d54e5ad7cb
Zach Hoobler <ZHoobler@main.agmcontainer.com>
2018-09-25 11:57:05 -0700

    Completed change to INTs, added different pins for arduino/attiny

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,82 @@
 #include <Arduino.h>
-#include <math.h>
-#include <HardwareSerial.h>
+
+
+/* 
+    define pins depending on target platform. Most testing has been done on the Arduino nano, but final design uses an attiny85.
+    VALVE_PIN - controls motor ground for valve - must be on for any movement
+    RELAY_PIN - triggers motor reverse relay - ON = close valve
+    KNOB_PIN - potentiometer input for dash knob, divider between 5v and gnd
+    VBATT_PIN - voltage divider input for time adjustment
+*/
+
+#ifdef ARDUINO_AVR_NANO
+    #define VALVE_PIN 2
+    #define RELAY_PIN 3
+    #define KNOB_PIN A0
+    #define VBATT_PIN A1
+#endif
+
+#ifdef ARDUINO_AVR_ATTINYX5
+    #define VALVE_PIN 0
+    #define RELAY_PIN 1
+    #define KNOB_PIN A1
+    #define VBATT_PIN A2
+#endif
 
 // Tuning variables - these all need to be adjusted for proper functionality
-//const float knobMin = 0.7;
-const int knobMin = 70;
-//const float knobMax = 1.0;
-const int knobMax = 100;
+const int knobRawMin = 0;       // min adc value expected for knob, between 0 and 1024
+const int knobRawMax = 1024;    // max adc value expected
 
-const float vBattDivider = 3.0;     // voltage divider factor, determine experimentally after final assembly
-const float vBattMin = 8.0;
+const int vBattDivider = 3000;      // voltage divider factor, determine experimentally after final assembly
+const int vBattMin = 8000;          // minimum voltage for operation (don't bother if below 8000mv)
 
-const int closeTimeAt10V = 4900;   // time in milliseconds for valve to completely close at 10v
-const int closeTimeAt14V = 3400;   // time in milliseconds for valve to completely close at 14v
+const int closeTimeAt10V = 4900;    // time in milliseconds for valve to completely close at 10v
+const int closeTimeAt14V = 3400;    // time in milliseconds for valve to completely close at 14v
 
 
-// pin settings
-const int valvePin = 2;       // controls motor ground for valve. needed for all movement
-const int relayPin = 3;       // triggers motor reverse relay - ON = close valve
-const int knobRawPin = A0;    // potentiometer input for dash knob
-const int vBattRawPin = A1;   // voltage divider input for time adjustment
+
+
+
+// pin settings from above definitions
+const int valvePin = VALVE_PIN;
+const int relayPin = RELAY_PIN;
+const int knobRawPin = KNOB_PIN;
+const int vBattRawPin = VBATT_PIN;
 
 
 // Global variables and a few constants that shouldn't need adjustment
 
-int knobPosition = 0;     // knob position as a float between 0 and 1, saves some calls to getKnobPosition
-int valvePosition = 0;    // valve position as a float between 0 and 1, needed to check for knob movement
-float vBatt = 0.0;            // current battery voltage. Refreshes every main loop
+int knobPosition = 0;     // knob position as an int between 0 and 100, saves some calls to getKnobPosition
+int valvePosition = 0;    // valve position as an int between 0 and 100, needed to check for knob movement
+int vBatt = 0;            // current battery voltage in mv - Refreshes every main loop
 unsigned long t = 0;
-const float closeTimeM = (closeTimeAt14V-closeTimeAt10V)/(14.0-10.0);
-const float closeTimeB = closeTimeAt14V - closeTimeM*14.0;
+const int closeTimeM = (closeTimeAt14V-closeTimeAt10V)/14;
+const int closeTimeB = closeTimeAt14V - closeTimeM*14;
 
 
-float getVBatt() {
-    //  vBatt = ADC value (0-1023) * 5v max value * voltage divider factor / 1024.0 (10-bit adc, decimal for float conversion)
-    return analogRead(vBattRawPin)*5*vBattDivider/1024.0;
+int getVBatt() {
+    //  vBatt = ADC value (0-1023) * 5v max value * voltage divider factor / 1024
+    return analogRead(vBattRawPin)*5*vBattDivider/1024;
 }
 
-float getValveTime(float vBatt) {
+int getValveTime(int vBatt) {
     //  valveTime is a linear approximation of the time it takes to close the valve at a given voltage
-    return closeTimeM*vBatt+closeTimeB;
+    return closeTimeM*vBatt/1000+closeTimeB;
 }
 
 
-//  Return knob position as a float between 0 and 1
+//  Return knob position as a percentage between 0 and 100
 int getKnobPosition() {
     //int knobPosition = analogRead(knobRawPin)*100;
     //knobPosition = knobPosition/1024;
     //return knobPosition;
 
-    return 70;
+    //return 70;
 
 
 
     //return abs((analogRead(knobRawPin)*100)-knobMin*1024)/(knobMax-knobMin)*1024;
+    return abs(analogRead(knobRawPin)-knobRawMin)*100/(knobRawMax-knobRawMin);
 }
 
 // Zero the valve completely closed, then deactivates it. Uses getValveTime(), which uses getVBatt()
@@ -62,21 +85,23 @@ void closeValve() {
     digitalWrite(valvePin, HIGH);
 }
 
-void openValve(float position){
+void openValve(int percentage){
     digitalWrite(valvePin, HIGH);
     digitalWrite(relayPin, LOW);
-    delay(getValveTime(getVBatt())*position);
+    delay(getValveTime(getVBatt())*percentage/100);
     digitalWrite(valvePin, LOW);
 }
 
 void setup() {
-    Serial.begin(9600);
+    //Serial.begin(9600);
     pinMode(valvePin, OUTPUT);   
     pinMode(relayPin, OUTPUT);   
 }
 
 
 /*
+
+Serial code for testing variables. Less needed now that main loop is functioning properly. Has not been updated for change to int.
 void loop() {
     Serial.println("getVBatt " + String(getVBatt()));
     valvePosition = getValveTime(getVBatt());
@@ -95,31 +120,30 @@ void loop() {
 
 void loop() {
     vBatt = getVBatt();
-    Serial.println("vBatt:" + String(vBatt));
+    //Serial.println("vBatt:" + String(vBatt));
     if (vBatt > vBattMin){
         knobPosition = getKnobPosition();
-        Serial.println("knobPosition:" + String(knobPosition));
+        //Serial.println("knobPosition:" + String(knobPosition));
         if (abs(knobPosition-valvePosition) > 2){         // if knob position differs from expected valve position by >2%, initiate valve repositioning
             valvePosition = knobPosition;
-            Serial.println("knob moved:" + String(knobPosition));
+            //Serial.println("knob moved:" + String(knobPosition));
             closeValve();
             t = millis();
-            Serial.println("closing valve, time t = " + String(t));
+            //Serial.println("closing valve, time t = " + String(t));
             while (millis() < t + 5000 && millis() < t + getValveTime(getVBatt())) {
-                Serial.println(String(millis()) + " < t+5000 = " + (String(t+5000)));
+                //Serial.println(String(millis()) + " < t+5000 = " + (String(t+5000)));
                 if (abs(valvePosition - getKnobPosition()) > 2) {  // if the knob has moved, reset the counter
                     t = millis();
                     delay(5);
                     valvePosition = getKnobPosition();;
-                    Serial.println("knob moved, new t =" + String(t));
+                    //Serial.println("knob moved, new t =" + String(t));
                 }
                 delay(50);
             }
-            Serial.println("loop broken, opening to " + String(valvePosition));
+            //Serial.println("loop broken, opening to " + String(valvePosition));
             delay(5);
-            openValve(valvePosition/100.0);
+            openValve(valvePosition);
         }
     }
     delay(10);
 }
-
